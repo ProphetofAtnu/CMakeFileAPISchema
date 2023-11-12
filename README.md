f57a336da2c51fa9edb6ff64648a05e4218b5fdd
Jacob Scaggs <jacscaggs@gmail.com>
2023-11-12 14:17:57 -0500

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# CMakeFileApiSchemas
+
+JSON Schema draft v7 declarations for the CMake File API.
+
---
+++ validate.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+
+check-jsonschema --schemafile ./schema/cache-v2.json tests/cache-v2-*.json
+check-jsonschema --schemafile ./schema/cmakeFiles-v1.json tests/cmakeFiles-v1-*.json
+check-jsonschema --schemafile ./schema/codemodel-v2-directory.json tests/directory-*.json
+check-jsonschema --schemafile ./schema/codemodel-v2-target.json tests/target-*.json
+check-jsonschema --schemafile ./schema/codemodel-v2.json tests/codemodel-v2-*.json
+check-jsonschema --schemafile ./schema/toolchains-v1.json tests/toolchains-v1-*.json
